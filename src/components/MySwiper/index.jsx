9e0fd2fee8f5ccdfb3ab633e8084f7a2d429536c
十 <welllon710>
2021-02-25 22:00:09 +0800

    25wan

--- src/components/MySwiper/index.jsx
+++ src/components/MySwiper/index.jsx
@@ -1,53 +1,55 @@
 import ColumnGroup from "antd/lib/table/ColumnGroup";
-import React, { useEffect, useMemo, useState, useRef, useCallback } from "react";
+import React, {
+  useEffect,
+  useMemo,
+  useState,
+  useRef,
+  useCallback,
+} from "react";
 import { useSelector } from "react-redux";
 import "./index.scss";
 export default function Swiper() {
   const [state, setstate] = useState([
     {
       id: 1,
-      img: "/assets/0.jpg",
+      img: "/assets/IMG_4966.jpg",
     },
     {
       id: 2,
-      img: "/assets/1.jpg",
+      img: "/assets/IMG_4976.jpg",
     },
     {
       id: 3,
-      img: "/assets/2.jpg",
+      img: "/assets/IMG_5093.jpg",
     },
     {
       id: 4,
-      img: "/assets/3.jpg",
+      img: "/assets/IMG_5376.jpg",
     },
     {
       id: 5,
-      img: "/assets/4.jpg",
+      img: "/assets/IMG_5395.jpg",
     },
     {
       id: 6,
-      img: "/assets/5.jpg",
+      img: "/assets/IMG_5399.jpg",
     },
     {
       id: 7,
-      img: "/assets/6.jpg",
+      img: "/assets/IMG_5407.jpg",
     },
     {
       id: 8,
-      img: "/assets/7.jpg",
+      img: "/assets/IMG_5411.jpg",
     },
   ]);
   const [currentW, setCurrentW] = useState(762);
   const [offsetL, setOffsetL] = useState(222);
-  const width = useSelector((state) => {
-    return state.width;
-  });
-  const refwidth = useRef();
+  const width = useSelector((state) => state.width);
   const [styleList, setStyleList] = useState([]);
   let a = 540;
-
+  let timeOut = null;
   useMemo(() => {
-    console.log("执行usememo");
     setStyleList((styleList) => {
       state.map((item, index) => {
         let i = index * 1 + 1;
@@ -77,87 +79,82 @@ export default function Swiper() {
       return styleList;
     });
   }, [state]);
-  useEffect(() => {
+  useMemo(() => {
     setCurrentW((currentW) => {
-        currentW = refwidth.current.clientWidth
-        console.log('currentW',currentW);
-      return currentW;
+      if (width <= 1200) {
+        return (currentW = 762);
+      } else {
+        return width - 1200 + 762;
+      }
     });
-  
   }, [width]);
-  useMemo(() => {
-    setOffsetL((offsetL) => {
-        offsetL = currentW - 540
-        console.log('offsetL',offsetL);
-      return offsetL;
-    });
-  }, [currentW])
-  useMemo(() => {
-    setStyleList(styleList=>{
-    console.log('重新渲染');
-       let arr =  JSON.parse(JSON.stringify(styleList))
-       arr.map((item,index)=>{
-            let i = index * 1 + 1;
-            if(i <= 3){
-                item.transform = `translateX(${test(i) + "px"})${   
-                    i == 2 ? "" : "scale(0.85)"
-                  }`
-                item.zIndex = `${i == 2 ? "2" : "1"}`
-            }else{
-                a += offsetL;
-                item.transform = `translateX(${a + "px"}) scale(0.85)`
-            }
-            return arr
-        })
-        console.log('c',arr);
-        
-        return arr
-    })
-    
-    
+  const xin = useMemo(() => {
+    if (currentW - 540 <= 340) {
+      return currentW - 540;
+    } else {
+      return 640;
+    }
   }, [currentW]);
-  const test = useCallback((num)=>{
-      console.log('useCall',offsetL);
-    switch (num) {
+  useMemo(() => {
+    setStyleList((styleList) => {
+      let arr = JSON.parse(JSON.stringify(styleList));
+      arr.map((item, index) => {
+        let i = index * 1 + 1;
+        if (i <= 3) {
+          item.transform = `translateX(${test(i) + "px"})${
+            i == 2 ? "" : "scale(0.85)"
+          }`;
+          item.zIndex = `${i == 2 ? "2" : "1"}`;
+        } else {
+          a += offsetL;
+          item.transform = `translateX(${a + "px"}) scale(0.85)`;
+          delete item.zIndex;
+        }
+        return arr;
+      });
+
+      return arr;
+    });
+  }, [currentW]);
+  const test = useCallback(
+    (num) => {
+      switch (num) {
         case 1:
           return 0;
         case 2:
-            console.log('当前2222-》',offsetL);
-          return offsetL / 2; // 2倍关系 最高300->600   最低130->260   760-540/2 = 110
+          return xin / 2;
         case 3:
-          return offsetL;
+          return xin;
       }
-  },[offsetL])
-//   const test = (num) => {
-//     switch (num) {
-//       case 1:
-//         return 0;
-//       case 2:
-//           console.log('当前2222-》',offsetL);
-//         return offsetL / 2; // 2倍关系 最高300->600   最低130->260   760-540/2 = 110
-//       case 3:
-//         return offsetL;
-//     }
-//   };
-  // useEffect(() => {
-  //     const timeOut = setInterval(() => {
-  //         setStyleList(styleList=>{
-  //             const _len = styleList.length
-  //             styleList = [styleList[_len - 1],...styleList]
-  //             styleList.pop()
-  //             return styleList
-  //         })
-  //     }, 2000);
-  //     return () => {
-  //         clearInterval(timeOut)
-  //     }
-  // })
+    },
+    [currentW]
+  );
+  useEffect(() => {
+    console.log("重新旋转");
+    timeOut = tt();
+    return () => {
+      clearInterval(timeOut);
+    };
+  }, []);
+  useEffect(() => {
+    clearInterval(timeOut);
+  }, [width]);
+  const tt = () => {
+    const timeOut = setInterval(() => {
+      setStyleList((styleList) => {
+        const _len = styleList.length;
+        styleList = [styleList[_len - 1], ...styleList];
+        styleList.pop();
+        return styleList;
+      });
+    }, 2000);
+    return timeOut;
+  };
 
   return (
     <div className="my-swiper">
       <ul
         style={{ width: width <= 1200 ? "762px" : width - 1200 + 762 + "px" }}
-        ref={refwidth}
       >
         {state.map((item, index) => {
           return (
