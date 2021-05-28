ece029e9f05882d795e36938f0fb1d45fed29ce5
welllon710 <18697703903@163.com>
2021-05-28 16:35:16 +0800

    摸鱼写的

--- src/pages/Details/com/Mv/index.jsx
+++ src/pages/Details/com/Mv/index.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useMemo, useRef } from "react";
 import { Image } from "antd";
+import { useSize } from "ahooks";
 import { RightSquareOutlined } from "@ant-design/icons";
-import './index.scss'
+import "./index.scss";
 export const Mv = ({ w }) => {
-    let h = Math.floor(parseInt(w) / 2) + '%'
-    return (
-      <div className="mv-2" style={{width:w,height:'180px'}} >
-        <div className="mv-img">
-          <Image
-            width={'100%'}
-            height={'100%'}
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-          />
-          <div className="right-play">
-                    <RightSquareOutlined />
-                    <span>2221万</span>
-          </div>
-          <div className="right-time">03:56</div>
+  const ref = useRef();
+  const size = useSize(ref);
+  let h = useMemo(() => {
+    return Math.floor(size.width / 2) + 40 - 0;
+  }, [size.width]);
+  console.log("h", h);
+  return (
+    <div
+      className="mv-2"
+      style={{ width: w, height: h + "px", marginRight: "20px" }}
+      ref={ref}
+    >
+      <div className="mv-img">
+        <Image
+          width={"100%"}
+          height={h - 40 + "px"}
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        />
+        <div className="right-play">
+          <RightSquareOutlined />
+          <span>2221万</span>
         </div>
-        <div className="mv-name">
-          <div>光年之外</div>
-          <p>G.E.M邓紫棋</p>
-        </div>
+        <div className="right-time">03:56</div>
       </div>
-    );
-}
+      <div className="mv-name">
+        <div>光年之外</div>
+        <p>G.E.M邓紫棋</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/Details/searchDetail.jsx
+++ src/pages/Details/searchDetail.jsx
@@ -1,27 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { useHeight } from "../../my-hooks/useHeight";
 import { Single } from "./com/Single";
 import { Singer } from "./com/Singer";
 import { Mv } from "./com/Mv";
+import { useWidth } from "../../my-hooks/_width";
 import { Tabs } from "antd";
 import "./searchDetail.scss";
 const { TabPane } = Tabs;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 export const SearchDetail = () => {
+  const [currentW, width] = useWidth(); //动态长度22
+  console.log("currentW", currentW);
+  let w = useMemo(() => {
+    if (currentW < 1100 && currentW > 800) {
+      return "32%";
+    } else if (currentW >= 1100) {
+      return "24%";
+    } else if (currentW < 800) {
+      return "24%";
+    }
+  }, [currentW]);
   useHeight();
   return (
     <div className="search-detail">
       <div className="top">
         <h2>找到713首单曲</h2>
-        <TopTabs />
+        <TopTabs w={w} />
       </div>
     </div>
   );
 };
-
-const TopTabs = () => {
-  const callback = () => {
-    console.log("点我");
-  };
+const TopTabs = ({ w }) => {
+  const callback = () => {};
   return (
     <Tabs defaultActiveKey="1" onChange={callback}>
       <TabPane tab="单曲" key="1">
@@ -37,13 +47,14 @@ const TopTabs = () => {
       </TabPane>
       <TabPane tab="视频" key="4">
         <div className="mv-item">
-          <Mv w={"23%"}  />
-          <Mv w={"23%"}  />
-          <Mv w={"23%"}  />
-          <Mv w={"23%"}  />
-          <Mv w={"23%"}  />
+          {arr.map((item, index) => {
+            return setMv(index);
+          })}
         </div>
       </TabPane>
     </Tabs>
   );
+};
+const setMv = () => {
+  return <div></div>;
 };
